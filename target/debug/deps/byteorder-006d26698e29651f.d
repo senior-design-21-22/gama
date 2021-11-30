70b6e9e0b15c0cd0320fdd7bfe9d0472189d00d9
AlexHeffner <alexjheffner@gmail.com>
2021-11-29 22:15:02 -0500

    Half of OS done

--- target/debug/deps/byteorder-006d26698e29651f.d
+++
@@ -1,8 +0,0 @@
-/mnt/c/Users/Alex Heffner/Documents/Org/4Senior/Senior1/CS4560/gama/target/debug/deps/byteorder-006d26698e29651f.rmeta: /home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/lib.rs /home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/io.rs
-
-/mnt/c/Users/Alex Heffner/Documents/Org/4Senior/Senior1/CS4560/gama/target/debug/deps/libbyteorder-006d26698e29651f.rlib: /home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/lib.rs /home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/io.rs
-
-/mnt/c/Users/Alex Heffner/Documents/Org/4Senior/Senior1/CS4560/gama/target/debug/deps/byteorder-006d26698e29651f.d: /home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/lib.rs /home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/io.rs
-
-/home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/lib.rs:
-/home/alex/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.4.3/src/io.rs:
--- target/debug/gama.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Alex\ Heffner/Documents/Org/4Senior/Senior1/CS4560/gama/target/debug/gama: /mnt/c/Users/Alex\ Heffner/Documents/Org/4Senior/Senior1/CS4560/gama/src/main.rs
